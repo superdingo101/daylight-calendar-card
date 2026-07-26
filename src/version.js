1ad9b984a8f138d506090137cad7bc2e200514e9
superdingo101 <103346430+superdingo101@users.noreply.github.com>
2026-07-26 00:09:12 +0000

    Prepare release v4.10.0

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const DAYLIGHT_CALENDAR_CARD_VERSION = 'v4.9.1';
+export const DAYLIGHT_CALENDAR_CARD_VERSION = 'v4.10.0';
 
 export function getDaylightCalendarCardVersion() {
   return DAYLIGHT_CALENDAR_CARD_VERSION.includes('__')
