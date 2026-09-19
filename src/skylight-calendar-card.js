867aa91573fb90d4c67d281c0b67f9b7961e0d9d
superdingo101 <103346430+superdingo101@users.noreply.github.com>
2026-09-18 20:47:01 -0700

    Fix modal-safe theme and hass renders (#586)

--- src/skylight-calendar-card.js
+++ src/skylight-calendar-card.js
@@ -417,6 +417,10 @@ class SkylightCalendarCard extends HTMLElement {
       }
 
       this._isDarkMode = !!event.matches;
+      if (this.isEventManagementDialogOpen()) {
+        this._pendingHeaderSensorRender = true;
+        return;
+      }
       this.render();
     };
     this._weekStandardFixedOffsetHeight = null;
@@ -1114,7 +1118,12 @@ class SkylightCalendarCard extends HTMLElement {
     this.refreshWeatherForecastData();
 
     if (shouldRender) {
-      this.renderPreservingAgendaScroll();
+      if (this.isEventManagementDialogOpen()) {
+        this._pendingHeaderSensorRender = true;
+      } else {
+        this._pendingHeaderSensorRender = false;
+        this.renderPreservingAgendaScroll();
+      }
     }
 
     // Refresh only when stale or when current view needs dates outside loaded range.
